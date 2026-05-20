89fdefb9a19b0dde42db9dca49f4837a5eee18c7
Shivani Kadirgamarajah <shivanikadirgamrajah@gmail.com>
2026-05-20 06:08:44 -0400

    removed popup

--- my-portfolio/app/page.tsx
+++ my-portfolio/app/page.tsx
@@ -1,26 +1,22 @@
 "use client";
 
-import React, { useRef } from "react";
+import React from "react";
 import HeroSection from "./sections/HeroSection";
 import SkillsSection from "./sections/SkillsSection";
 import AboutSection from "./sections/AboutSection";
 import ExperienceSection from "./sections/ExperienceSection";
 import GoalsSection from "./sections/GoalsSection";
 import ResumeSection from "./sections/ResumeSection";
-import UniversityPopup from "./UniversityPopup";
 
 export default function Home() {
-  const aboutRef = useRef<HTMLDivElement>(null);
-
   return (
     <>
       <HeroSection />
-      <SkillsSection ref={aboutRef} />
+      <SkillsSection />
       <AboutSection />
       <ExperienceSection />
       <GoalsSection />
       <ResumeSection />
-      <UniversityPopup targetRef={aboutRef} />
     </>
   );
 }
